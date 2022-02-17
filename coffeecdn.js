b9ecb60f52d5d3cb787130c2665a98c3a4ae7830
Soumya Mondal <62131036+lifeofsoumya@users.noreply.github.com>
2022-02-17 11:51:34 +0530

    comments added for future ref.

--- coffeecdn.js
+++ coffeecdn.js
@@ -1,22 +1,22 @@
-class MyCoffee extends HTMLElement {
+class MyCoffee extends HTMLElement { // MyCoffee is the portion containing html code to be entered in the tag
     connectedCallback() {
-        this.innerHTML = `
+        this.innerHTML = ` 
         <div class="coffee-box">
         <a target="_blank" href="https://soumyamondal.com/coffee"><img
                 style="position:fixed; bottom: 15px; left: 15px; z-index: 9999;" width="150px"
                 src="https://soumyamondal.com/img/general/getmecoffee.png"
                 alt="Buy me a coffee"></a>
         </div>
-        `;
+        `; // html code ends here
     }
 }
 
-customElements.define('my-coffee', MyCoffee);
+customElements.define('my-coffee', MyCoffee); // my-coffee is a custom html tag which can be called using <my-coffee>
 
-        function callCoffee() {
-            var h = document.getElementsByTagName("body")[0]; var s = document.createElement("my-coffee");
-            var newScript = document.createElement('script');
-            document.head.appendChild(newScript);
-            h.appendChild(s);
+        function callCoffee() { // function to insert the html content inside the html file
+            var h = document.getElementsByTagName("body")[0]; var s = document.createElement("my-coffee"); // gets the body and creates an tag element automatically 
+            var newScript = document.createElement('script'); // automatially creates a script inside the html file
+            document.head.appendChild(newScript); // inserts the newly created script to the head of the html file
+            h.appendChild(s); // inserts the custom tag in the body of the file
         };
 callCoffee();
